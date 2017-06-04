527d4692e8b04fb73188856db3532ee16e9bcd49
mkhsojib <mkhsojib@gmail.com>
2017-06-05 04:24:41 +0600

    Setting up Relations

--- app/User.php
+++ app/User.php
@@ -23,4 +23,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    public function address()
+    {
+        return $this->hasOne('App\Address');
+    }
 }
